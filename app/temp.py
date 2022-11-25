288ad6b4f730c5ab2f09df30da524ff20169864b
Rudra Barot <rudrabarot2015@gmail.com>
2022-11-25 13:02:14 +0530

    7th kanda added

--- app/temp.py
+++ app/temp.py
@@ -1,11 +1,7 @@
-import json
+structure = {
 
-with open("app/data/ramayan2.json","r") as f:
-    d = json.load(f)
+    "content":[
+        
+    ]
 
-with open("app/data/ramayan.json","w+") as f:
-    json.dump(d,f)
-
-
-# a = "तपःस्वाध्यायनिरतं तपस्वी वाग्विदां वरम् |\nनारदं परिपप्रच्छ वाल्मीकिर्मुनिपुङ्गवम् || १-१-१"
-
+}
