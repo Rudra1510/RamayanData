2483231c1c397a78075589d8b556cb3356a620fb
Rudra Barot <rudrabarot2015@gmail.com>
2022-11-28 12:01:01 +0530

    adding subindex in the index file

--- index.py
+++ index.py
@@ -30,7 +30,78 @@ def File(Path):
         return flask.send_from_directory("", Path)
     except Exception as e:
         return f"{type(e).__name__}"
+    
+
+@app.route("/main")
+def Main():
+    try:
+        return flask.send_from_directory("","data/assets/index.json")
+    except Exception as e:
+        return f"{type(e).__name__}"
+    
+@app.route("/chapter/<path:Bkno>/<path:Chno>/<path:Lang>")
+def chapter(Bkno,Chno,Lang):
+    Chno = int(Chno) - 1
+    
+    with open(f"data/assets/books/{Bkno}.json","r") as f:
+        d = json.load(f)
+
+    if Lang == "all":
+        return d["content"][Chno]
+
+    else:
+        Languages = Lang.split("-")
+        OutputJSON = {}
+        OutputJSON["mainTitle"] = d["content"][Chno]["mainTitle"]
+        OutputJSON["content"] = []
+        OutputChapters = []
+        for i in range(len(d["content"][Chno]["content"])):
+            chapter = {}
+            chapter["title"] = d["content"][Chno]["content"][i]["title"]
+            for l in Languages:
+                chapter[l] = d["content"][Chno]["content"][i][l]
+            OutputChapters.append(chapter)
+        OutputJSON["content"] = OutputChapters
+
+        return OutputJSON
+    
+
+@app.route("/index")
+def index():
+
+    with open("data/assets/index.json","r") as f:
+        d = json.load(f)
+
+    
+        
+    for i in range(1,len(d["books"])+1):
+
+        d["books"][i-1]["index"] = {}
+
+        with open(f"data/assets/books/{str(i)}.json","r") as f:
+            b = json.load(f)
+
+            for j in range(len(b["content"])):
+
+                shlokaTitles = []
+                for k in b["content"][j]["content"]:
+                    shlokaTitles.append(k["title"])
+
+                d["books"][i-1]["index"][b["content"][j]["mainTitle"]] = shlokaTitles
+
+    with open("temp.json","w+") as f:
+        json.dump(d,f,indent=4)
+
+
+        
+
+        # i["index"] = 0
+
+    
+    
+
+    return "hey"
 
     
 if __name__=="__main__":
-    app.run()
+    app.run(port=5051)
